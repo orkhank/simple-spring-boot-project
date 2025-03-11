45d3baed59f04fca955d4f09d526e822295f66d2
orkhank <118054233+orkhank@users.noreply.github.com>
2025-03-11 13:56:28 +0300

    implement `ProductService`

---
+++ src/main/java/com/example/demo/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> fetchProductList() {
+        return (List<Product>) productRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void updateProduct(Product product, Long productId) {
+        productRepository.findById(productId).ifPresent(product1 -> {
+            product1.setName(product.getName());
+            product1.setPrice(product.getPrice());
+
+            productRepository.save(product1);
+        });
+    }
+
+    @Override
+    public void deleteProductById(Long productId) {
+        productRepository.deleteById(productId);
+    }
+}
