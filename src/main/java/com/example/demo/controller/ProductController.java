68223bc4eae42e2271be41c0110a534ef530357d
orkhank <118054233+orkhank@users.noreply.github.com>
2025-03-11 13:56:52 +0300

    create REST controller for `Product`

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/")
+    public Product saveProduct(@Validated @RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping("/")
+    public List<Product> fetchDepartmentList() {
+        return productService.fetchProductList();
+    }
+
+    @PutMapping("/{id}")
+    public void updateProduct(@RequestBody Product product, @PathVariable("id") Long productId) {
+        productService.updateProduct(product, productId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable("id") Long id) {
+        productService.deleteProductById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
