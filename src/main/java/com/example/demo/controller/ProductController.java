2efc2c532762eeaa6e4538dd1c6c081857ce4aea
orkhank <118054233+orkhank@users.noreply.github.com>
2025-03-13 15:29:05 +0300

    implement caching at controller level using IMap

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
+import com.hazelcast.map.IMap;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,17 +17,32 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
+    private final IMap<Long, Product> productCache;
 
-    public ProductController(ProductService productService) {
+    public ProductController(
+            ProductService productService,
+            @Qualifier("productCache") IMap<Long, Product> productCache) {
         this.productService = productService;
+        this.productCache = productCache;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> fetchProduct(@PathVariable("id") Long productId) {
+        // check if the product is cached
+        if (productCache.containsKey(productId)) {
+            return ResponseEntity.ok(productCache.get(productId));
+        }
+
+        // only access db if the product is not cached
         Optional<Product> fetched = productService.getProduct(productId);
 
-        return fetched.map(ResponseEntity::ok)
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (fetched.isPresent()) {
+            Product product = fetched.get();
+            productCache.set(productId, product);
+            return ResponseEntity.ok(product);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/")
@@ -40,12 +57,16 @@ public class ProductController {
 
     @PutMapping("/{id}")
     public void updateProduct(@RequestBody Product product, @PathVariable("id") Long productId) {
-        productService.updateProduct(product, productId);
+        Optional<Product> updatedProduct = productService.updateProduct(product, productId);
+        if (updatedProduct.isPresent() && productCache.containsKey(productId)) {
+            productCache.set(productId, updatedProduct.get());
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProductById(@PathVariable("id") Long id) {
         productService.deleteProductById(id);
+        productCache.delete(id);
 
         return ResponseEntity.noContent().build();
     }
