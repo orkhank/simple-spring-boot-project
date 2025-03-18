a08509102dd1aaa57241a74c39927f62f1e925ab
orkhank <118054233+orkhank@users.noreply.github.com>
2025-03-18 10:24:35 +0300

    add bean for cachedProductService

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.example.demo.config;
 
+import com.example.demo.CachedProductService;
 import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +16,12 @@ public class AppConfig {
     @Bean
     public IMap<Long, Product> productCache(HazelcastInstance instance) {
         return instance.getMap("map");
+    }
+
+    @Bean
+    public ProductService cachedProductService(
+            ProductService productService,
+            @Qualifier("productCache") IMap<Long, Product> productCache) {
+        return new CachedProductService(productService, productCache);
     }
 }
