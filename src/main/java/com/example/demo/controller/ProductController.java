140d0250f14b706fd6c7399d8a8071732bbc6635
orkhank <118054233+orkhank@users.noreply.github.com>
2025-03-18 10:27:27 +0300

    make controller depend on cachedProductService
    and mapping methods accordingly (remove logic about caching)

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
-import com.hazelcast.map.IMap;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -17,28 +16,17 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
-    private final IMap<Long, Product> productCache;
 
-    public ProductController(
-            ProductService productService,
-            @Qualifier("productCache") IMap<Long, Product> productCache) {
+    public ProductController(@Qualifier("cachedProductService") ProductService productService) {
         this.productService = productService;
-        this.productCache = productCache;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> fetchProduct(@PathVariable("id") Long productId) {
-        // check if the product is cached
-        if (productCache.containsKey(productId)) {
-            return ResponseEntity.ok(productCache.get(productId));
-        }
-
-        // only access db if the product is not cached
         Optional<Product> fetched = productService.getProduct(productId);
 
         if (fetched.isPresent()) {
             Product product = fetched.get();
-            productCache.set(productId, product);
             return ResponseEntity.ok(product);
         }
 
@@ -57,16 +45,12 @@ public class ProductController {
 
     @PutMapping("/{id}")
     public void updateProduct(@RequestBody Product product, @PathVariable("id") Long productId) {
-        Optional<Product> updatedProduct = productService.updateProduct(product, productId);
-        if (updatedProduct.isPresent() && productCache.containsKey(productId)) {
-            productCache.set(productId, updatedProduct.get());
-        }
+        productService.updateProduct(product, productId);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProductById(@PathVariable("id") Long id) {
         productService.deleteProductById(id);
-        productCache.delete(id);
 
         return ResponseEntity.noContent().build();
     }
